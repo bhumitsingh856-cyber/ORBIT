4340b2085420b8aacd17e78159efef90c3f6c2e2
unknown <bhumitsingh856@gmail.com>
2026-01-18 16:53:22 +0530

    env port added

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -19,7 +19,7 @@ import { Server } from "socket.io";
 import { initializeSocket } from "./socket/socket.js";
 db();
 const app = express();
-const port = 3000;
+const port =process.env.PORT || 3000;
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
